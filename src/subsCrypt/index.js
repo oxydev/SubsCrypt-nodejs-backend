8ccb456b15cd3562bae27c79449943645a487324
amirsadra79 <58373783+amirsadra79@users.noreply.github.com>
2021-04-28 14:25:03 +0430

    filling functions (except functions that needs sender)

--- src/subsCrypt/index.js
+++ src/subsCrypt/index.js
@@ -5,14 +5,23 @@ import subscrypt from 'subscrypt';
 const errors = require('../errors');
 
 
-exports.checkAuth = (req, res, next) => {
+exports.checkAuth = async (req, res, next) => {
     // check user authorization
+    let user = req['user']
+    let provider_address = req['provider_address']
+    let token = req['token']
+    let pass_phrase = req['phrase']
+    const response = await subscrypt.checkAuth(user, provider_address, token, pass_phrase)
 
 };
 
-exports.retrieveWholeDataWithPassword = (req, res, next) => {
+exports.retrieveWholeDataWithPassword = async (req, res, next) => {
     // Retrieve whole data with password
     // (user: string, token: String, phrase: String) -> Vec<SubscriptionRecord>
+    let user = req['user']
+    let token = req['token']
+    let phrase = req['phrase']
+    const response = await subscrypt.retrieveDataWithPassword(user, token, phrase)
 
 };
 
@@ -22,21 +31,31 @@ exports.retrieveWholeDataWithWallet = (req, res, next) => {
 
 };
 
-exports.retrieveDataWithPassword = (req, res, next) => {
+exports.retrieveDataWithPassword = async (req, res, next) => {
     // Retrieve data with password
     // (user: string, provider_address: string, token: String, phrase: String) -> Vec<SubscriptionRecord>
+    let user = req['user']
+    let provider_address = req['provider_address']
+    let token = req['token']
+    let phrase = req['phrase']
+    const response = await subscrypt.retrieveDataWithPassword(user, provider_address, token, phrase)
 
 };
 
-exports.retrieveDataWithWallet = (req, res, next) => {
+exports.retrieveDataWithWallet = async (req, res, next) => {
     // Retrieve data with wallet
 
 
+
 };
 
-exports.checkSubscription = (req, res, next) => {
+exports.checkSubscription = async (req, res, next) => {
     // check subscription
     // (user: string, provider_address: string, plan_index: u128) -> boolean
+    let user = req['user']
+    let provider_address = req['provider_address']
+    let plan_index = req['plan_index']
+    const response = await subscrypt.checkSubscription(user, provider_address, plan_index)
 
 
 };
