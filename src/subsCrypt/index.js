c291ff1794005af4e20aa84fdb79476c1f0e9fb2
amirsadra79 <58373783+amirsadra79@users.noreply.github.com>
2021-04-29 02:05:21 +0430

    preparing functions

--- src/subsCrypt/index.js
+++ src/subsCrypt/index.js
@@ -27,8 +27,6 @@ exports.retrieveWholeDataWithPassword = async (req, res, next) => {
 
 exports.retrieveWholeDataWithWallet = (req, res, next) => {
     // Retrieve whole data with wallet
-
-
 };
 
 exports.retrieveDataWithPassword = async (req, res, next) => {
@@ -44,9 +42,6 @@ exports.retrieveDataWithPassword = async (req, res, next) => {
 
 exports.retrieveDataWithWallet = async (req, res, next) => {
     // Retrieve data with wallet
-
-
-
 };
 
 exports.checkSubscription = async (req, res, next) => {
@@ -56,6 +51,61 @@ exports.checkSubscription = async (req, res, next) => {
     let provider_address = req['provider_address']
     let plan_index = req['plan_index']
     const response = await subscrypt.checkSubscription(user, provider_address, plan_index)
+};
 
+exports.checkSubscriptionWithUsername = async (req, res, next) => {
+    let username = req['username']
+    let provider_address = req['provider_address']
+    let phrase = req['phrase']
+    const response = await subscrypt.checkSubscriptionWithUsername(username, provider_address, phrase)
 
 };
+exports.getUsernameByAddress = async (req, res, next) => {
+    let address = req['address']
+    const response = await subscrypt.getUsernameByAddress(address)
+
+};
+exports.retrieveDataWithUsername = async (req, res, next) => {
+    let username = req['username']
+    let provider_address = req['provider_address']
+    let phrase = req['phrase']
+    const response = await subscrypt.retrieveDataWithUsername(username, provider_address, phrase)
+
+};
+
+exports.getPlanData = async (req, res, next) => {
+    let provider_address = req['provider_address']
+    let plan_index = req['plan_index']
+    const response = await subscrypt.getPlanData(provider_address, plan_index)
+
+};
+exports.retrieveWholeDataWithUsername = async (req, res, next) => {
+    let username = req['username']
+    let phrase = req['phrase']
+    const response = await subscrypt.retrieveWholeDataWithUsername(username, phrase)
+
+};exports.isUsernameAvailable = async (req, res, next) => {
+    let username = req['username']
+    const response = await subscrypt.isUsernameAvailable(username)
+
+};
+exports.userCheckAuthWithUsername = async (req, res, next) => {
+    let username = req['username']
+    let pass_phrase = req['pass_phrase']
+    const response = await subscrypt.userCheckAuthWithUsername(username, pass_phrase)
+
+};
+exports.providerCheckAuthWithUsername = async (req, res, next) => {
+    let username = req['username']
+    let pass_phrase = req['pass_phrase']
+    const response = await subscrypt.providerCheckAuthWithUsername(username, pass_phrase)
+};
+
+exports.checkAuthWithUsername = async (req, res, next) => {
+    let username = req['username']
+    let provider_address = req['provider_address']
+    let pass_phrase = req['pass_phrase']
+    const response = await subscrypt.checkAuthWithUsername(username, provider_address, pass_phrase)
+
+};
+
