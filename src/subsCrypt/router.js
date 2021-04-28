8e11283b1b1826ce3c03515baa123e14fa29147d
amirsadra79 <58373783+amirsadra79@users.noreply.github.com>
2021-04-29 02:05:02 +0430

    update router

--- src/subsCrypt/router.js
+++ src/subsCrypt/router.js
@@ -11,6 +11,15 @@ router.get('/', tasks.retrieveWholeDataWithWallet);
 router.get('/', tasks.retrieveDataWithPassword);
 router.get('/', tasks.retrieveDataWithWallet);
 router.get('/', tasks.checkSubscription);
+router.get('/', tasks.checkSubscriptionWithUsername);
+router.get('/', tasks.getUsernameByAddress);
+router.get('/', tasks.retrieveDataWithUsername);
+router.get('/', tasks.getPlanData);
+router.get('/', tasks.retrieveWholeDataWithUsername);
+router.get('/', tasks.isUsernameAvailable);
+router.get('/', tasks.userCheckAuthWithUsername);
+router.get('/', tasks.providerCheckAuthWithUsername);
+router.get('/', tasks.checkAuthWithUsername);
 
 // Export the router
 module.exports = router;
