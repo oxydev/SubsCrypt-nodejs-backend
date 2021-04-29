2967d0fcc5795857c4fe0fc8d153a4d5338e20e3
amirsadra79 <58373783+amirsadra79@users.noreply.github.com>
2021-04-29 23:36:56 +0430

    update main router

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,10 @@
 'use strict';
 
 const router = require('express').Router();
-const middleware = require('./middleware');
 const errors = require('./errors');
-const healthRouter = require('./health/router');
-const taskRouter = require('./tasks/router');
 const subsCryptRouter = require('./subsCrypt/router');
 
-// Wire up middleware
-router.use(middleware.doSomethingInteresting);
-
 // Wire up routers
-router.use('/health', healthRouter);
-router.use('/tasks', taskRouter);
 router.use('/subsCrypt', subsCryptRouter);
 
 // Wire up error-handling middleware
